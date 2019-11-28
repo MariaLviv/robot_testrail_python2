ca2116c48e374b9ea136d196b7e565910c7433bb
Maria <maria.shyrko@schindler.com>
2019-11-28 05:09:12 -0800

    package files created

--- setup.py
+++ setup.py
@@ -1,23 +1,39 @@
-from distutils.core import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
+
+
 setup(
-  name = 'robot_testrail_python2',
-  packages = ['robot_testrail_python2'],
-  version = '0.1',
-  license='Apache-2.0',
-  description = 'Integration Robot Framework with TestRail for python 2.7',
-  author = 'Maria Shyrko',
-  author_email = 'shirko_2000@yahoo.com',
-  url = 'https://github.com/user/reponame',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
-  keywords = ['ROBOT', 'TESTRAIL'],
-  install_requires=[
-          'requests'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Integration Tools',
-    'License :: OSI Approved :: Apache-2.0 License',
-    'Programming Language :: Python :: 2.7',
-  ],
+    name='robot_testrail_python2',
+    packages=['robot_testrail_python2'],
+    version='0.1',
+    license='Apache-2.0',
+    description='Integration Robot Framework with TestRail for python 2.7',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Maria Shyrko',
+    author_email='shirko_2000@yahoo.com',
+    url='https://github.com/MariaLviv/robot_testrail_python2',
+    keywords=['testing', 'testautomation', 'robotframework', 'testrail', 'robot'],
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Robot Framework :: Library',
+    ],
 )
+
